3bad330ebdcef0cf73b1bf4a442ec1a1b7ce572c
Ljan31 <108371723+Ljan31@users.noreply.github.com>
2024-10-04 20:20:40 -0400

    Add files via upload

---
+++ 1/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administración de Catastros</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="bg-primary text-white text-center py-4">
+        <h1>Municipio de La Paz</h1>
+        <h2>Autoridad Catastral Municipal</h2>
+    </div>
+
+    <?php
+    include('./nav.php');
+    ?>
+
+    <div class="container mt-4" id="info-general">
+        <h2>Información General</h2>
+        <p>La administración de catastros se centra en la gestión y mantenimiento de registros de propiedades y tierras dentro del municipio. Su objetivo es asegurar un control preciso y actualizado de la información catastral, facilitando así la planificación urbana, la recaudación de impuestos y la protección de derechos de propiedad.</p>
+    </div>
+
+    <footer class="text-center mt-4 py-3 bg-light">
+        <p>&copy; 2024 Administración de Catastros. Todos los derechos reservados.</p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ 1/nav.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+    <a class="navbar-brand" href="index.php">HAM-LP</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="tipos-tramite.php">Tipos de Trámites</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="normativa.php">Normativa</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="faq.php">FAQ</a>
+            </li>
+        </ul>
+    </div>
+</nav>
